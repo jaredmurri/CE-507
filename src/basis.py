718bdacd3b11f673334963034204a93c4907dd9b
jaredmurri <113555740+jaredmurri@users.noreply.github.com>
2022-09-19 18:32:53 -0600

    first problem correct

---
+++ src/basis.py
@@ -0,0 +1,48 @@
+from random import vonmisesvariate
+import unittest
+import math
+import numpy
+
+def evalLegendreBasis1D(degree, variate):
+    if degree==0:
+        val=1
+    elif degree==1:
+        val=variate
+    else:
+        n=degree-1
+        Pn=evalLegendreBasis1D(n,variate)
+        Pnminus1=evalLegendreBasis1D(n-1,variate)
+        term1=((2*n)+1) *variate*Pn
+        term2=n*Pnminus1
+        term3= n+1
+        val=(term1-term2)/term3
+    return val
+
+
+
+
+class Test_evalLegendreBasis1D( unittest.TestCase ):
+    def test_basisAtBounds( self ):
+        for p in range( 0, 2 ):
+            if ( p % 2 == 0 ):
+                self.assertAlmostEqual( first = evalLegendreBasis1D( degree = p, variate = -1 ), second = +1.0, delta = 1e-12 )
+            else:
+                self.assertAlmostEqual( first = evalLegendreBasis1D( degree = p, variate = -1 ), second = -1.0, delta = 1e-12 )
+            self.assertAlmostEqual( first = evalLegendreBasis1D( degree = p, variate = +1 ), second = 1.0, delta = 1e-12 )
+
+    def test_constant( self ):
+        for x in numpy.linspace( -1, 1, 100 ):
+            self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 0, variate = x ), second = 1.0, delta = 1e-12 )
+
+    def test_linear( self ):
+        for x in numpy.linspace( -1, 1, 100 ):
+            self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 1, variate = x ), second = x, delta = 1e-12 )
+
+    def test_quadratic_at_roots( self ):
+        self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 2, variate = -1.0 / math.sqrt(3.0) ), second = 0.0, delta = 1e-12 )
+        self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 2, variate = +1.0 / math.sqrt(3.0) ), second = 0.0, delta = 1e-12 )
+
+    def test_cubic_at_roots( self ):
+        self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 3, variate = -math.sqrt( 3 / 5 ) ), second = 0.0, delta = 1e-12 )
+        self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 3, variate = 0 ), second = 0.0, delta = 1e-12 )
+        self.assertAlmostEqual( first = evalLegendreBasis1D( degree = 3, variate = +math.sqrt( 3 / 5 ) ), second = 0.0, delta = 1e-12 )
